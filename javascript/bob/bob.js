54a16438cb81856dcdac29cdc294c70a01fc1139
Eric Stensland <estensland88@gmail.com>
2014-04-05 08:43:14 -0500

    finished bob.js

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,21 +1,36 @@
 var Bob = function() {
   this.hey = function(input){
-    if (input.trim() === "") {
+    if (silenceCheck(input)) {
       return "Fine. Be that way!";
     }
-    else if(input === input.toUpperCase() && input.match(/[a-zA-Z]/)){
+    else if(screamingCheck(input)){
       return "Woah, chill out!";
     }
-    else if(input.trim().slice(-1) === "?"){
+    else if(questionCheck(input)){
       return "Sure.";
     }
     else {
       return "Whatever.";
     }
-
   };
   
-  
+  var silenceCheck = function(input){
+    return input.trim() === "";
+  };
+
+  var screamingCheck = function(input) {
+    if (input === input.toUpperCase()){
+      return letterCheck(input);
+    }
+  };
+
+  var letterCheck = function(input) {
+    return input.match(/[a-zA-Z]/);
+  };
+
+  var questionCheck = function(input) {
+    return input.trim().slice(-1) === "?";
+  };
 };
 
 module.exports = Bob;
