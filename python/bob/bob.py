143b49432f4e4727e9dd2feec3d1e1dba50c2b2b
Eric Stensland <estensland88@gmail.com>
2014-04-05 21:31:44 -0500

    refactored to modularize bob.py

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,12 +1,22 @@
 import re 
 
 class Bob:
+
   def hey(self, input):
-    if input.rstrip() == "":
+    if self.silence_check(input):
       return "Fine. Be that way!"
-    elif input == input.upper() and re.search('[A-Z]', input):
+    elif self.scream_check(input):
       return "Woah, chill out!"
-    elif input.rstrip()[-1] == "?":
+    elif self.question_check(input):
       return "Sure."
     else:
       return "Whatever." 
+
+  def silence_check(self, input):
+    return input.rstrip() == ""
+
+  def scream_check(self, input):
+    return input == input.upper() and re.search('[A-Z]', input)
+
+  def question_check(self, input):
+    return input.rstrip()[-1] == "?"
