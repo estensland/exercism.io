cec6a3e49bff748196b219eeb7fd6e5df2808357
Eric Stensland <estensland88@gmail.com>
2014-04-05 21:24:21 -0500

    viable code python bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,6 +1,12 @@
+import re 
+
 class Bob:
   def hey(self, input):
-    if input == input.upper():
+    if input.rstrip() == "":
+      return "Fine. Be that way!"
+    elif input == input.upper() and re.search('[A-Z]', input):
       return "Woah, chill out!"
+    elif input.rstrip()[-1] == "?":
+      return "Sure."
     else:
       return "Whatever." 
