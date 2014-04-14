3eff4a19b0ef8e4b716b3a0c8705ea460122e065
Eric Stensland <estensland88@gmail.com>
2014-04-14 12:02:54 -0500

    iteration 3 bob.rb

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,28 +1,28 @@
 class Bob
   def hey(user_input)
-    return "Fine. Be that way!" if is_silent?(user_input)
-    return "Woah, chill out!" if is_screaming?(user_input)
-    return "Sure." if is_question?(user_input)
+    return "Fine. Be that way!" if silent?(user_input)
+    return "Woah, chill out!" if screaming?(user_input)
+    return "Sure." if question?(user_input)
     return "Whatever."
   end
 
-  def is_silent?(user_input)
+  def silent?(user_input)
     user_input.rstrip == ""
   end
 
-  def is_screaming?(user_input)
-    is_all_upcase?(user_input) && has_letters?(user_input)
+  def screaming?(user_input)
+    all_upcase?(user_input) && letters?(user_input)
   end
 
-  def is_question?(user_input)
+  def question?(user_input)
     user_input.rstrip.end_with?("?")
   end
 
-  def is_all_upcase?(user_input)
+  def all_upcase?(user_input)
     user_input == user_input.upcase
   end
 
-  def has_letters?(user_input)
+  def letters?(user_input)
     user_input =~ /[a-zA-Z]/
   end
 
