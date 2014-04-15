c61215c28d3a0686de7176e565c0a8cedac6dd99
Eric Stensland <estensland88@gmail.com>
2014-04-15 09:31:13 -0500

    refactoring refactoring refactoring bob.js

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,36 +1,37 @@
 var Bob = function() {};
 
-Bob.prototype.toneHeard = {
+Bob.prototype= {
+  toneHeard : {
+    isSilent : function(input){
+      return input.trim() === "";
+    },
 
-  isSilent : function(input){
-    return input.trim() === "";
+    isScreaming : function(input) {
+      return this.isUpcase(input) && this.hasLetters(input);
+    },
+
+    isUpcase : function(input) {
+      return input === input.toUpperCase();
+    },
+
+    hasLetters : function(input) {
+      return input.match(/[a-zA-Z]/);
+    },
+
+    isQuestion : function(input) {
+      return input.trim().slice(-1) === "?";
+    },
   },
 
-  isScreaming : function(input) {
-    return this.isUpcase(input) && this.hasLetters(input);
-  },
+  hey : function(input){
+    if (this.toneHeard.isSilent(input)) return "Fine. Be that way!";
 
-  isUpcase : function(input) {
-    return input === input.toUpperCase();
-  },
+    else if(this.toneHeard.isScreaming(input)) return "Woah, chill out!";
 
-  hasLetters : function(input) {
-    return input.match(/[a-zA-Z]/);
-  },
+    else if(this.toneHeard.isQuestion(input))return "Sure.";
 
-  isQuestion : function(input) {
-    return input.trim().slice(-1) === "?";
-  },
-};
-
-Bob.prototype.hey = function(input){
-  if (this.toneHeard.isSilent(input)) return "Fine. Be that way!";
-  
-  else if(this.toneHeard.isScreaming(input)) return "Woah, chill out!";
-  
-  else if(this.toneHeard.isQuestion(input))return "Sure.";
-  
-  else return "Whatever.";
+    else return "Whatever.";
+  }
 };
 
 module.exports = Bob;
