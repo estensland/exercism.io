c009da800c795e8bbb6c5a549777e516694359d5
Eric Stensland <estensland88@gmail.com>
2014-04-15 08:14:42 -0500

    refactoring js

---
+++ clojure/bob/bob.clj
@@ -0,0 +1,6 @@
+(ns bob)
+
+(defn response-for [user-input]
+  "Whatever.")
+
+; In progress
--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,38 +1,41 @@
-var Bob = function() {
-  this.hey = function(input){
-    if (isSilent(input)) {
-      return "Fine. Be that way!";
-    }
-    else if(isScreaming(input)){
-      return "Woah, chill out!";
-    }
-    else if(isQuestion(input)){
-      return "Sure.";
-    }
-    else {
-      return "Whatever.";
-    }
-  };
+var Bob = function() {}
+
+
+Bob.prototype.hey = function(input){
+  if (isSilent(input)) {
+    return "Fine. Be that way!";
+  }
+  else if(isScreaming(input)){
+    return "Woah, chill out!";
+  }
+  else if(isQuestion(input)){
+    return "Sure.";
+  }
+  else {
+    return "Whatever.";
+  }
+};
   
-  var isSilent = function(input){
+Bob.prototype.tones = {
+  isSilent : function(input){
     return input.trim() === "";
-  };
+  },
 
-  var isScreaming = function(input) {
+  isScreaming : function(input) {
     return isUpcase(input) && hasLetters(input);
-  };
+  },
 
-  var isUpcase = function(input) {
+  isUpcase : function(input) {
     return input === input.toUpperCase();
-  };
+  },
 
-  var hasLetters = function(input) {
+  hasLetters : function(input) {
     return input.match(/[a-zA-Z]/);
-  };
+  },
 
-  var isQuestion = function(input) {
+  isQuestion : function(input) {
     return input.trim().slice(-1) === "?";
-  };
+  },
 };
 
 module.exports = Bob;
