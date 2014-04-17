0d607799d1b371cfed45af8eef5d5c091073b0bc
Eric Stensland <estensland88@gmail.com>
2014-04-17 11:30:47 -0500

    pass tests for bob.clj

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -1,8 +1,17 @@
 (ns bob)
 
-(defn response-for [userInput]
+(defn is-scream [user-input]
+  (if (and (= user-input (.toUpperCase user-input)) 
+    (not= user-input (.toLowerCase user-input))) 
+      true
+      false))
+
+(defn is-question [user-input]
+  (.endsWith user-input "?"))
+
+(defn response-for [user-input]
   (cond
-   (empty? (.trim userInput))  "Fine. Be that way!"
-   (= userInput (.toUpperCase userInput) and ) "Woah, chill out!"
-   (.endsWith userInput "?") "Sure."
-   :else              "Whatever."))
+    (empty? (.trim user-input))  "Fine. Be that way!"
+    (is-scream (.trim user-input))"Woah, chill out!"
+    (is-question (.trim user-input))"Sure."
+    :else "Whatever."))
