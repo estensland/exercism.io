03e1a5e80c1ab3196d53899c7ab08cb9a2c63fd3
Eric Stensland <estensland88@gmail.com>
2014-04-17 09:49:44 -0500

    refactored bob.js to remove if else crapola

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,15 +1,8 @@
 var Bob = function() {};
 
 Bob.prototype = {
-  toneHeard : {
-    isSilent : function(input){
-      return input.trim() === "";
-    },
 
-    isScreaming : function(input) {
-      return this.isUpcase(input) && this.hasLetters(input);
-    },
-
+  toneHeardFormats :  {
     isUpcase : function(input) {
       return input === input.toUpperCase();
     },
@@ -17,20 +10,39 @@ Bob.prototype = {
     hasLetters : function(input) {
       return input.match(/[a-zA-Z]/);
     },
+  },
+
+  toneHeard : {
+    isSilent : function(input){
+      return input.trim() === "";
+    },
+
+    isScreaming : function(input) {
+      return Bob.prototype.toneHeardFormats.isUpcase(input) && Bob.prototype.toneHeardFormats.hasLetters(input);
+    },
 
     isQuestion : function(input) {
       return input.trim().slice(-1) === "?";
     },
+
+    isWhatever : function(input) {
+      return true;
+    },
+  },
+
+  responsesTo: {
+    isSilent : "Fine. Be that way!",
+    isScreaming : "Woah, chill out!",
+    isQuestion : "Sure.",
+    isWhatever : "Whatever."
   },
 
   hey : function(input){
-    if (this.toneHeard.isSilent(input)) return "Fine. Be that way!";
-
-    else if(this.toneHeard.isScreaming(input)) return "Woah, chill out!";
-
-    else if(this.toneHeard.isQuestion(input))return "Sure.";
-
-    else return "Whatever.";
+    for(object in this.toneHeard){
+      if (this.toneHeard[object](input)) {
+        return (this.responsesTo[object]);
+      }
+    }
   }
 };
 
