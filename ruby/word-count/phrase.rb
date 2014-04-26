8f62e9ad9f2767a9e7cbaa6981868b50e87f457b
Eric Stensland <estensland88@gmail.com>
2014-04-26 09:37:25 -0500

    initial attempt at word-count.rb

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,7 +1,17 @@
 class Phrase 
 
-  def initialize
+  def initialize(words)
+    @words = words
+  end
 
+  def word_count
+    words.split(" ").each_with_object({}) do |count, word|
+      if count[word]
+        count[word] += 1
+      else
+        count[word] = 1
+      end
+    end
   end
 
 end
