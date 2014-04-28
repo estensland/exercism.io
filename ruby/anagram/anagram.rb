8b4311156c151a474cbd74c384e3e0b774bc96af
Eric Stensland <estensland88@gmail.com>
2014-04-28 18:09:15 -0500

    refactored anagram.rb

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -2,21 +2,17 @@ class Anagram
   attr_reader :word
 
   def initialize(word)
-    @word = word
+    @word = word.downcase
   end
 
   def match(options)
-    matches = []
-    options.each do |option|
-      next if word.downcase == option.downcase
-      sorted_word = word.downcase.split('').sort.join('')
+    sorted_word = word.downcase.split('').sort.join('')
+  
+    options.select do |option|
+      next if word == option.downcase
       sorted_option = option.downcase.split('').sort.join('')
-      matches << option if sorted_word == sorted_option
+      option if sorted_word == sorted_option
     end
-    matches
   end
 
 end
-
-detector = Anagram.new('Orchestra')
-p detector.match %w(cashregister Carthorse radishes)
