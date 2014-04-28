071459221fb87dd15da5d81bb9a7c011e5562329
Eric Stensland <estensland88@gmail.com>
2014-04-28 18:44:12 -0500

    refactored to helper methods

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,25 +1,29 @@
+var AnagramHelpers = {
+  anagramCheck : function(self, option){return option.toLowerCase() === self.sortedWord;},
+
+  sameWordCheck : function(self, option){return self.word === option;},
+
+  sortWord : function(word) {return word.toLowerCase().split('').sort().join('');},
+
+};
+
 var Anagram = function(word) {
   this.word = word.toLowerCase();
-  this.sortedWord = this.word.split('').sort().join('');
+  this.sortedWord = word.toLowerCase().split('').sort().join('');
 };
 
 Anagram.prototype = {
-  anagramCheck : function(option){return option.toLowerCase() === this.sortedWord},
-
-  sameWordCheck : function(option){}
 
   match: function(options) {
-    results = []
+    results = [];
     for(var index = 0; index < options.length; index ++){
-      sortedOption = options[index].toLowerCase().split('').sort().join('');
+      sortedOption = AnagramHelpers.sortWord(options[index]);
 
-      if (this.word === options[index].toLowerCase()) {continue;}
-      if (this.anagramCheck(sortedOption)) {results.push(options[index]); }
-    } 
+      if (AnagramHelpers.sameWordCheck(this, options[index].toLowerCase())) {continue;}
+      if (AnagramHelpers.anagramCheck(this, sortedOption)) {results.push(options[index]); }
+    }
     return results;
   }
-
-  
 };
 
 module.exports = Anagram;
