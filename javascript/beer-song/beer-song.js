0ac460c9ec95b3b31dae277b5b994af5e60b3b36
Eric Stensland <estensland88@gmail.com>
2014-04-28 23:27:20 -0500

    refactored beer-song.js

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,29 +1,27 @@
 Beer = {
   lineOne: function(round) {
-    if (round === 1) {
-      return round + " bottle of beer on the wall, " + round + " bottle of beer.\n";
-    }
-    else if (round === 0) {
-      return "No more bottles of beer on the wall, no more bottles of beer.\n";
-    }
-    else {
-      return round + " bottles of beer on the wall, " + round + " bottles of beer.\n";
-    }
+    if (round === 1) {phrase = round + " bottle of beer"; }
+    
+    else if (round === 0) {phrase = "No more bottles of beer"; }
+    
+    else {phrase = round + " bottles of beer"; }
+    
+    return phrase + " on the wall, " + phrase.toLowerCase() + ".\n";
   },
 
   lineTwo: function(round) {
     if (round === 1) {
       return "Take it down and pass it around, no more bottles of beer on the wall.\n";
     }
+    
     else if (round === 0) {
       return "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
     }
-    else if (round === 2) {
-      phrase = "1 bottle";
-    }
-    else {
-      phrase = (round - 1) + " bottles";
-    }
+    
+    else if (round === 2) {phrase = "1 bottle"; }
+    
+    else {phrase = (round - 1) + " bottles"; }
+    
     return "Take one down and pass it around, " + phrase + " of beer on the wall.\n";
   },
 
@@ -32,12 +30,14 @@ Beer = {
   },
 
   sing : function(start, end) {
-    if(end !== parseInt(end)) {end = 0};
+    if(end !== parseInt(end)) {end = 0;}
     result = [];
+    
     while(start >= end) {
       result.push(this.verse(start));
       start --;
     }
+
     return result.join("\n");
   }
 };
