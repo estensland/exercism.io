aba50bb28a002505a1993993726cc5d36ff342a3
Eric Stensland <estensland88@gmail.com>
2014-04-28 19:48:18 -0500

    set up for better code

--- javascript/beer-song/beer-song.js
+++ javascript/beer-song/beer-song.js
@@ -1,18 +1,39 @@
 Beer = {
+  lineOne: function(round) {
+    if (round === 1) {
+      return round + " bottle of beer on the wall, " + round + " bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
+    }
+    else if (round === 0) {
+      return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
+    }
+    else {
+      
+    }
+  }
   verse : function(round) {
-    return round + " bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, " + (round -1) + " bottles of beer on the wall.\n"
+    if (round === 1) {
+      return round + " bottle of beer on the wall, " + round + " bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
+    }
+    else if (round === 0) {
+      return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
+    }
+    else if (round === 2) {
+      return round + " bottles of beer on the wall, "+ round + " bottles of beer.\nTake one down and pass it around, " + (round -1) + " bottle of beer on the wall.\n";
+    }
+    else {
+      return round + " bottles of beer on the wall, "+ round + " bottles of beer.\nTake one down and pass it around, " + (round -1) + " bottles of beer on the wall.\n";
+    }
   },
 
-  sing : function(number) {
+  sing : function(start, end) {
+    if(end !== parseInt(end)) {end = 0} 
     result = [];
-    while(number >= 0) {
-      result.push(this.verse(number));
-      number --;
+    while(start >= end) {
+      result.push(this.verse(start));
+      start --;
     }
-    return result.join("");
+    return result.join("\n");
   }
 };
 
 module.exports = Beer;
-
-var expected = "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n";
