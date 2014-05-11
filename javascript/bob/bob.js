425f6b18e6dda8036dc9f551f1165c6a4e4450f0
Eric Stensland <estensland88@gmail.com>
2014-05-10 21:06:55 -0500

    removed tone helpers from bob in bob.js

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,35 +1,33 @@
 var Bob = function() {};
 
+var toneHelpers = {
+  isUpcase : function(input) {
+    return input === input.toUpperCase();
+  },
+
+  hasLetters : function(input) {
+    return input.match(/[a-zA-Z]/);
+  },
+
+  isSilent : function(input){
+    return input.trim() === "";
+  },
+
+  isScreaming : function(input) {
+    var formats = Bob.prototype.toneHeardFormats;
+    return formats.isUpcase(input) && formats.hasLetters(input);
+  },
+
+  isQuestion : function(input) {
+    return input.trim().slice(-1) === "?";
+  },
+
+  isWhatever : function(input) {
+    return true;
+  },
+};
+
 Bob.prototype = {
-
-  toneHeardFormats :  {
-    isUpcase : function(input) {
-      return input === input.toUpperCase();
-    },
-
-    hasLetters : function(input) {
-      return input.match(/[a-zA-Z]/);
-    },
-  },
-
-  toneHeard : {
-    isSilent : function(input){
-      return input.trim() === "";
-    },
-
-    isScreaming : function(input) {
-      var formats = Bob.prototype.toneHeardFormats;
-      return formats.isUpcase(input) && formats.hasLetters(input);
-    },
-
-    isQuestion : function(input) {
-      return input.trim().slice(-1) === "?";
-    },
-
-    isWhatever : function(input) {
-      return true;
-    },
-  },
 
   responseOrder: ["isSilent", "isScreaming", "isQuestion", "isWhatever"],
 
