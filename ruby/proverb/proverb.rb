58b6203ffa6b6c14bd4ece737665fa56e5e22503
Eric Stensland <estensland88@gmail.com>
2014-05-11 15:06:52 -0500

    added ternary operator to prover.rb

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -1,6 +1,6 @@
 class Proverb
   def initialize(*args, qualifier: nil)
-    @qualifier = qualifier || " "
+    @qualifier = qualifier
     @phrase = args_to_phrase(args)
   end
 
@@ -10,13 +10,12 @@ class Proverb
 
   def args_to_phrase(*args)
     phrase = ""    
-  
 
     args.flatten!.each_with_index do |word, index|
       unless index == args.length - 1
         phrase << "For want of a #{word} the #{args[index + 1].to_s} was lost.\n"
       else
-        phrase << "And all for the want of a #{@qualifier} #{args.first}."    
+        phrase << "And all for the want of a #{@qualifier ? @qualifier + " " : nil}#{args.first}."    
       end
     end
 
